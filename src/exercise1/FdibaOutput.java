82e2b1190394540243c1a2c77cf67dcd1dea20ee
SkyDoge69 <luc2002penta@gmail.com>
2021-10-14 07:57:49 +0300

    Added FdibaOutput file

---
+++ src/exercise1/FdibaOutput.java
@@ -0,0 +1,14 @@
+package exercise1;
+
+public class FdibaOutput {
+    public static void main(String[] args) {
+        String result = """
+                FFFFF DDDDD   II BBBBB      A
+                F     D    D  II B    B    A A
+                FFFF  D     D II BBBBB    AAAAA
+                F     D    D  II B    B  A     A
+                F     DDDDD   II BBBBB  A       A
+                """;
+        System.out.println(result);
+    }
+}
