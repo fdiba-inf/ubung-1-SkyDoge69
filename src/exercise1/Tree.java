2c780a61fb899b0d3c1ffb568e8ecd833c44e5bf
SkyDoge69 <luc2002penta@gmail.com>
2021-10-14 08:06:08 +0300

    Did it the good old way

--- src/exercise1/Tree.java
+++ src/exercise1/Tree.java
@@ -2,14 +2,12 @@ package exercise1;
 
 public class Tree {
     public static void main(String[] args) {
-        String result = """
-                    *
-                   ***
-                  *****
-                 *******
-                *********
-                   ***
-                """;
+        String result = "    *\n" +
+                "   ***\n" +
+                "  *****\n" +
+                " *******\n" +
+                "*********\n" +
+                "   ***\n";
         System.out.println(result);
     }
 }
