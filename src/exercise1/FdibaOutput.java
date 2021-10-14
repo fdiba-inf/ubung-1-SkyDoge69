aada2082a15f5e967b137fb8e39d629a7d5f7d79
SkyDoge69 <luc2002penta@gmail.com>
2021-10-14 08:04:13 +0300

    Did it the good old way

--- src/exercise1/FdibaOutput.java
+++ src/exercise1/FdibaOutput.java
@@ -2,13 +2,11 @@ package exercise1;
 
 public class FdibaOutput {
     public static void main(String[] args) {
-        String result = """
-                FFFFF DDDDD   II BBBBB      A
-                F     D    D  II B    B    A A
-                FFFF  D     D II BBBBB    AAAAA
-                F     D    D  II B    B  A     A
-                F     DDDDD   II BBBBB  A       A
-                """;
+        String result = "FFFFF DDDDD   II BBBBB      A\n" +
+                "F     D    D  II B    B    A A\n" +
+                "FFFF  D     D II BBBBB    AAAAA\n" +
+                "F     D    D  II B    B  A     A\n" +
+                "F     DDDDD   II BBBBB  A       A\n";
         System.out.println(result);
     }
 }
